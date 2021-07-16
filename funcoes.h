fd28f061434326767e9ef83300bd10014c1d3614
Victor <31494853+victorbuquera@users.noreply.github.com>
2021-07-16 15:01:13 -0300

    Declaração das funções

--- funcoes.h
+++ funcoes.h
@@ -7,7 +7,6 @@ void MostraListaCorpos(struct corpos *corp, int qtdCorpos);
 //CALCULOS
 void CalculoCentroMassa(struct corpos *corp, int qtdCorpos);
 float CalculaMediaMassa(struct corpos *corp, int qtdCorpos);
-
 int MaiorPosicaoX(int posicaoX[], int qtdCorpos, int indice);
 int MaiorPosicaoY(int posicaoY[], int qtdCorpos, int indice);
 void MontaMapa(struct corpos *corp, int qtdCorpos);
